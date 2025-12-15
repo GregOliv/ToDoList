65a1920bcfc1689856afcd118d87100530c03278
GregOliv <136775038+GregOliv@users.noreply.github.com>
2025-12-15 21:04:21 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
 
+// Fix for Vercel routing where URI segments are stripped relative to script location
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
 // Redirect to public folder
 require __DIR__ . '/../public/index.php';
